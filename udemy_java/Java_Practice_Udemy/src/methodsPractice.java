7f5d67b367244389efb1e6f77d18272927ef9ec6
Anthony Forca <freecodecamp4716@gmail.com>
2024-04-07 10:28:27 -0400

    java udemy practice

--- udemy_java/Java_Practice_Udemy/src/methodsPractice.java
+++ udemy_java/Java_Practice_Udemy/src/methodsPractice.java
@@ -6,8 +6,25 @@ public class methodsPractice {
         System.out.println(displayHighScorePosition("Tim", calculateHighScorePosition(500)));
         System.out.println(displayHighScorePosition("Tim", calculateHighScorePosition(100)));
         System.out.println(displayHighScorePosition("Tim", calculateHighScorePosition(25)));
-
+        System.out.println(toMilesPerHour(1.5));
+        System.out.println(toMilesPerHour(10.25));
+        System.out.println(toMilesPerHour(-5.6));
+        System.out.println(toMilesPerHour(25.42));
+        System.out.println(toMilesPerHour(75.114));
+        System.out.println(printConversion(95.75));
     }
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        long mphRounded;
+        if (kilometersPerHour < 0) {
+            return -1;
+        } else {
+            double mph = kilometersPerHour * 0.621371;
+            mphRounded = Math.round(mph);
+        }
+        return mphRounded;
+    }
+
 
     public static String displayHighScorePosition(String name, int position){
         return name + " managed to get into position " + position + " on the high score list.";
@@ -23,5 +40,19 @@ public class methodsPractice {
         }else {
             return 4;
         }
+    }
+
+    public static long printConversion(double kilometersPerHour){
+        if (kilometersPerHour < 0){
+            System.out.println("Invalid Value");
+        }else {
+            long roundedKm = Math.round(kilometersPerHour);
+            long mph = Math.round(roundedKm * 0.62137119);
+            System.out.println(kilometersPerHour + " km/h = " + mph + " mi/h");
+            System.out.println(mph);
+            System.out.println(kilometersPerHour);
+            System.out.println(roundedKm);
+        }
+        return 0;
     }
 }
