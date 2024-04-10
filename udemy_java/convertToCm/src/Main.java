1f40666f1ed695a6f11abc58089486b4d189e409
Anthony Forca <freecodecamp4716@gmail.com>
2024-04-09 20:53:53 -0400

    overloading methods challenges

--- udemy_java/convertToCm/src/Main.java
+++ udemy_java/convertToCm/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(convertToCentimeters(1));
         convertToCentimeters(5, 8);
+        System.out.println(getDurationString(6000));
     }
     public static double convertToCentimeters(int inches) {
         double heightInCm = inches * 2.54;
@@ -15,6 +16,18 @@ public class Main {
                 "height in cm is " + justCm);
         return justCm;
 
+    }
+
+    public static String getDurationString(int seconds) {
+        int hours = seconds / 3600;
+        int minutes = seconds - (hours * 60);
+        int remainingSeconds =
+        //return XXh YYm ZZs
+        return hours + "h " + minutes + "m " + seconds + "s";
+    }
+
+    public static int getDurationString(int minutes, int seconds) {
+        return seconds;
     }
 }
 
